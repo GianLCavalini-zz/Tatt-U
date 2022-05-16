ce94c2719a756f8c319098c1f2615eead379468e
Gian Lucca <giandacoll@gmail.com>
2022-05-16 12:35:38 -0300

    deploy

--- Models/User.Model.js
+++ Models/User.Model.js
@@ -1,6 +1,5 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 
-
 const userSchema = new Schema(
 	{
 		name: { type: String, required: true, trim: true },
@@ -30,30 +29,6 @@ const userSchema = new Schema(
 	{ timestamps: true }
 );
 
-const userSchema = new Schema({
-  name: { type: String, required: true, trim: true },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-  },
-  passwordHash: { type: String, required: true },
-  profilePicture: { type: String, default:""},
-  birthDate: { type: String, default:""},
-  city: { type: String, default:"", required: true},
-  country: {type: String, default: "", required: true},
-  state: { type: String, default:"", required: true},
-  role: { type: String, enum: ["ADMIN", "USER", "ARTIST"], default: "USER" },
-  followings: { type: Array, default:[]},
-  followers: { type: Array, default:[]},
-  post: { type: Array, default:[]},
-  contact: {type: Number, 
-  match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/}
-
-
-});
 
 
 const UserModel = model("User", userSchema);

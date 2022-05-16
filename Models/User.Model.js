f5dbb95c3648d333ffbe05578150e6574233051e
Gian Lucca <giandacoll@gmail.com>
2022-05-16 13:19:29 -0300

    atualização1

--- Models/User.Model.js
+++ Models/User.Model.js
@@ -1,34 +1,7 @@
 const { Schema, model, default: mongoose } = require("mongoose");
 
 
-const userSchema = new Schema(
-	{
-		name: { type: String, required: true, trim: true },
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-			trim: true,
-			match: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-		},
-		passwordHash: { type: String, required: true },
-		profilePicture: { type: String, default: "" },
-		birthDate: { type: String, default: "" },
-		city: { type: String, default: "", required: true },
-		country: { type: String, default: "", required: true },
-		state: { type: String, default: "", required: true },
-		role: { type: String, enum: ["ADMIN", "USER", "ARTIST"], default: "USER" },
-		followings: { type: Array, default: [] },
-		followers: { type: Array, default: [] },
-		post: { type: mongoose.Types.ObjectId, ref: "Post" },
-		contact: {
-			type: Number,
-			match: /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
-			required: true,
-		},
-	},
-	{ timestamps: true }
-);
+
 
 const userSchema = new Schema({
   name: { type: String, required: true, trim: true },

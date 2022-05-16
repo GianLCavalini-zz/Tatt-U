4c71d31a4b2a81e06b8ad2733e7e30c0f68824d9
Gian Lucca <giandacoll@gmail.com>
2022-05-16 14:22:37 -0300

    rota alternativa

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ app.use("/post", postRouter);
 const messageRouter = require("./Routes/message.routes");
 app.use("/chat", messageRouter)
 
+app.get("/", (req, res)=>{
+  return res.send("ta funcionando")
+}) 
+
 app.listen(Number(process.env.PORT), () => {
   console.log("Server up at port: ", process.env.PORT);
 });

a85a0902789fb735ef80d00e49b565e9517c9bdb
Gian Lucca <giandacoll@gmail.com>
2022-05-17 11:26:32 -0300

    atualização message route

--- Models/Message.model.js
+++ Models/Message.model.js
@@ -1,10 +1,23 @@
 const {Schema, model, default: mongoose} = require("mongoose");
 
-const messageSchema = new Schema({
-    owner: {type: mongoose.Types.ObjectId, ref: "User"},
-    message: {type: String, required: true, trim: true},
-    send: {type: String}
-})
+const messageSchema = new Schema(
+	
+    {
+        receiverId: {
+            type: String,
+        },
+        senderId: {
+            type: String,
+        },
+        messageImg: { type: String, default: "" },
+        text: {
+            type: String,
+        },
+    },
+    { timestamps: true },
+
+);
+
 
 const MessageModel = model("message", messageSchema);
 
--- Routes/message.routes.js
+++ Routes/message.routes.js
@@ -5,44 +5,42 @@ const isAuth = require("../Middlewares/isAuth");
 const attachCurrentUser = require("../Middlewares/attachCurrentUser");
 const MessageModel = require("../Models/Message.model");
 
-//CREATE NEW MESSAGE
 
-router.post('/new-message', isAuth, attachCurrentUser, async (req, res) => {
- 
- 
- 
- 
-    try{
+
+router.post("/:artistId", isAuth, attachCurrentUser, async (req, res) => {
+    
+    const loggedInUser = req.currentUser;
+    try {
+        const createdMessage = await MessageModel.create({
+		      	receiverId: req.params.artistId,
+            senderId: loggedInUser._id,
+            text: req.body.text,
+            messageImg: req.body.messageImg
+			
+		});
+      res.status(200).json(createdMessage);
       
-      const newMessage = await MessageModel.create(
-          {
-              owner: req.currentUser._id,
-              ...req.body,
-              send: new Date(Date.now())
-          }
-      );
-      return res.status(201).json(newMessage)
-
-  } catch(err){
-      console.error(err)
-      return res.status(500).json(err)
-  }
-})
-
-// get messages
-
-router.get("/messages/:userId", isAuth, attachCurrentUser, async (req, res) => {
-   
-    try{
-        
-        const message = await MessageModel.find({ owner: req.params.userId });
-
-        return res.status(200).json(message);
-    } catch(err) {
+      
+    } catch (err) {
         console.log(err)
-        res.status(500).json(err)    
-    } 
-})
+      res.status(500).json(err);
+    }
+  });
+
+  router.get("/", isAuth, attachCurrentUser, async (req, res) => {
+    const loggedInUser = req.currentUser;
+    try {
+      const messages = await MessageModel.find({
+        senderId: loggedInUser._id,
+      });
+      res.status(200).json(messages);
+      console.log(messages)
+    } catch (err) {
+        console.log(err)
+      res.status(500).json(err);
+    }
+  });
+
 
 // delete a message
 
@@ -64,11 +62,5 @@ router.delete("/delete-message/:messageId", isAuth, attachCurrentUser, async (re
 
 
 })
-
-
-
-
-
-
 
 module.exports = router;

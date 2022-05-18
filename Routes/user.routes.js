cad47712d0a32ac3187c091e4a6a98672e382748
Gian Lucca <giandacoll@gmail.com>
2022-05-18 10:34:13 -0300

    atualização5

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -200,7 +200,7 @@ router.get("/following-artists/", isAuth, attachCurrentUser, async (req, res) =>
 
   // get all users
 
-router.get("/getartists", async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const allArtists = await UserModel.find({});
 

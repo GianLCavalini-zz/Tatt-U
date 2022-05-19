fe75594db36f22319a2d23f103c71bf0545bbbf7
Gian Lucca <giandacoll@gmail.com>
2022-05-19 12:05:27 -0300

    atualização rotas following artist 13

--- Routes/followings.routes.js
+++ Routes/followings.routes.js
@@ -7,18 +7,18 @@ const attachCurrentUser = require("../Middlewares/attachCurrentUser");
 //get following artists
 router.get("/:userId", async (req, res) => {
     try {
-      const user = await UserModel.findById(req.params.userId);
-      const friends = await Promise.all(
-        user.followings.map((friendId) => {
-          return UserModel.findById(friendId);
-        })
-      );
-      let friendList = [];
-      friends.map((friend) => {
-        const { name, profilePicture } = friend;
-        friendList.push({ name, profilePicture });
-      });
-      res.status(200).json(friendList)
+      const user = await UserModel.finOne({_id: req.params.userId}).populate("followings");
+      //const friends = await Promise.all(
+      //  user.followings.map((friendId) => {
+       //   return UserModel.findById(friendId);
+       // })
+      //);
+     // let friendList = [];
+     // friends.map((friend) => {
+     //   const { name, profilePicture } = friend;
+     //   friendList.push({ name, profilePicture });
+    //  });
+      res.status(200).json({followings: user.followings})
     } catch (err) {
         console.log(err)
       res.status(500).json(err);
